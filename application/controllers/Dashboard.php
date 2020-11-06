79ec62d30e53cefe0a47b98ffc4f07459257d45c
iqbalatma <iqbalatma@users.noreply.github.com>
2020-11-06 22:18:18 +0700

    Inisiasi template dasar

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        // $this->load->view('template_dashboard/template_header.php');
+        // $this->load->view('template_dashboard/template_mainheader.php');
+        // $this->load->view('template_dashboard/template_sidebar.php');
+        // $this->load->view('v_dashboard');
+        // $this->load->view('template_dashboard/template_footer.php');
+        $data = [
+            'title'     => 'Dashboard',
+            'content'   => 'v_dashboard.php'
+        ];
+        $this->load->view('template_dashboard/template_wrapper', $data);
+    }
+}
