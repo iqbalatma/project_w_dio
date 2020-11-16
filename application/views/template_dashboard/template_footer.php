d0adba9ddf703d220715ee138757aa27f542707b
iqbalatma <iqbalatma@users.noreply.github.com>
2020-11-16 15:18:07 +0700

    Hapus jquery di footer

--- application/views/template_dashboard/template_footer.php
+++ application/views/template_dashboard/template_footer.php
@@ -32,8 +32,7 @@
         </script>
     <?php endif; ?>
     <!-- sweetalert end -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
     <!--   Core JS Files   -->
     <script src="<?= base_url(); ?>/../assets/Atlantis-Lite-master/assets/js/core/jquery.3.2.1.min.js"></script>
     <script src="<?= base_url(); ?>/../assets/Atlantis-Lite-master/assets/js/core/popper.min.js"></script>
