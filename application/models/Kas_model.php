4670da7bd6103991ef09d2eebc22b40217a9b4ca
Dio Ilham <apajabole1@gmail.com>
2020-12-29 16:45:27 +0700

    ! CRUD kas perusahaan done + css untuk datatables responsive

--- application/models/Kas_model.php
+++ application/models/Kas_model.php
@@ -21,13 +21,15 @@ class Kas_model extends CI_Model
    */
   public function set_new_kas($data)
   {
-    $now = now();
-    $createdAt = unix_to_human($now, true, 'europe');
+    $now        = now();
+    $createdAt  = unix_to_human($now, true, 'europe');
     
     // kas_code format string build
     $code  = ($data['add-type'] == 'kredit') ? 'K' : 'D'; // K= Kredit ; D = Debet
     $code .= mdate('/%y/%m/', $now); // kode untuk tahun dan bulan
 
+    // pprint($code);
+    // pprintd($data);
     // get last kas_code form table row
     $last_row          = $this->db->select('kas_code, final_balance')->order_by('id',"desc")->limit(1)->get($this->table);
     $lastKasCode       = $last_row->row()->kas_code;
@@ -68,7 +70,7 @@ class Kas_model extends CI_Model
     $data = array(
 		  "kas_code"        => $code,
 		  "title"           => $data['add-perihal'],
-		  "description"     => $data['add-ketetrangan'],
+		  "description"     => ($data['add-keterangan'] !== '') ? $data['add-keterangan'] : NULL,
 		  "date"            => $data['add-date'],
 		  "debet"           => $debet,
 		  "kredit"          => $kredit,
@@ -80,88 +82,18 @@ class Kas_model extends CI_Model
 		return $this->db->insert($this->table, $data);
   }
   
-  
-  
-  
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   /**
    * 
-   * Update product that already registered and still active (not deleted).
+   * Delete kas dara row, entirely
    * 
    * @param array $id
-   * Set the $id from the product id to fetch the data relatives to the id.
-   * @param array $data [8 data]
-   * The key and value in the array that will be inserted into the database.
-   * 
-   */
-  public function set_update_by_id($id, $data)
-  {
-    if (($data['edit-tipeupdate'] !== '+') && ($data['edit-tipeupdate'] !== '-'))
-    {
-      return FALSE;
-    }
-    $createdAt = unix_to_human(now(), true, 'europe');
-    // ('+' or '-') and (total stok to be inputted)
-    $operand  = $data['edit-tipeupdate'];
-    $total    = $data['edit-updatestok'];
-    $data = [
-		  "updated_at"    => $createdAt,
-      "updated_by"    => $data['edit-username'],
-    ];
-    // pprint($total);
-    // pprintd($operand);
-    // set data to table `quantity`
-    $this->db->set("quantity", "quantity {$operand} {$total}", FALSE);
-    $this->db->where('id', $id);
-    return $this->db->update($this->table, $data);
-
-
-
-    // $this->db->trans_start();
-    // $this->db->trans_complete();
-
-    // if ($this->db->trans_status() === FALSE)
-    // {
-    //   // flashdata untuk sweetalert
-    //   $this->session->set_flashdata('failed_message', 1);
-    //   $this->session->set_flashdata('title', "Input gagal!");
-    //   $this->session->set_flashdata('text', 'Data gagal diproses! Hubungi administrator segera.');
-    //   redirect(base_url( getBeforeLastSegment($this->modules, 2) ));
-    // }
-    // else
-    // {
-    //   return 1;
-    // }
-
-  }
-  
-  /**
-   * 
-   * Delete employee that already registered, but not the actual row data deletion,
-   * this is just updating "is_deleted" fields in the table from 0 to 1.
-   * 
-   * @param array $id
-   * Set the $id from the product id to fetch the data relatives to the id.
+   * Set the $id from the kas id to fetch the data relatives to the id.
    * 
    */
   public function set_delete_by_id($id)
   {
-    // echo '<pre>'; print_r($id); die;
-    $data = array(
-		  "is_deleted"   => 1,
-    );
     $this->db->where('id', $id);
-		return $this->db->update($this->table, $data);
+    return $this->db->delete($this->table);
   }
 
 
@@ -200,12 +132,8 @@ class Kas_model extends CI_Model
   public function get_all($select = '*')
   {
     $this->db->select($select);
-    $this->db->from("{$this->tb_product} AS p");
-    $this->db->join("{$this->table} AS pi", "pi.product_id = p.id");
-    $this->db->join("{$this->tb_store} AS s", "s.id = pi.store_id");
-    // $this->db->where("{$this->tb_product_composition}.product_id", $id);
-    $this->db->where("pi.is_deleted", 0);
-    $this->db->order_by("pi.id", 'ASC');
+    $this->db->from("{$this->table}");
+    $this->db->order_by("id", 'DESC');
     $query = $this->db->get();
     if ( $query->num_rows() > 0) {
       return $query->result_array();
@@ -235,85 +163,6 @@ class Kas_model extends CI_Model
     $query = $this->db->get();
     if ( $query->num_rows() == 1) {
       return $query->row();
-    }
-    return FALSE;
-  }
-  
-  /**
-   * 
-   * Get one role by the role unique id
-   * 
-   * @param string $id 
-   * Set the $id from the role id to fetch the data relatives to the id.
-   * @param string $select 
-   * Default value is '*', but you can input some string
-   * to select some table(s) name of your choice.
-   * 
-   */
-  public function get_role_by_id($id, $select = '*')
-  {
-    // get from tb_department
-    $this->db->select($select);
-    $this->db->from($this->table);
-    $this->db->join($this->tb_role, "{$this->tb_role}.id={$this->table}.role_id");
-    $this->db->where("{$this->table}.id", $id);
-    $query = $this->db->get();
-    if ( $query->num_rows() == 1) {
-      return $query->row();
-    }
-    return FALSE;
-  }
-  
-  /**
-   * 
-   * Get one store by the store unique id
-   * 
-   * @param string $id 
-   * Set the $id from the store id to fetch the data relatives to the id.
-   * @param string $select 
-   * Default value is '*', but you can input some string
-   * to select some table(s) name of your choice.
-   * 
-   */
-  public function get_store_by_id($id, $select = '*')
-  {
-    // get from tb_department
-    $this->db->select($select);
-    $this->db->from($this->table);
-    $this->db->join($this->tb_store, "{$this->tb_store}.id={$this->table}.store_id");
-    $this->db->where("{$this->table}.id", $id);
-    $query = $this->db->get();
-    if ( $query->num_rows() == 1) {
-      return $query->row();
-    }
-    return FALSE;
-  }
-    
-  // get 1 product_composition berdasarkan id
-  // masukkan parameter kedua sebagai nama kolom pada database, untuk select kolom
-  /**
-   * 
-   * Get all Product Composition that is belong to
-   * the corresponding Product by joining some tables on its id.
-   * 
-   * @param string $id 
-   * Set the $id from the product id to fetch the data relatives to the id.
-   * @param string $select 
-   * Default value is '*', but you can input some string
-   * to select some table(s) name of your choice.
-   * 
-   */
-  public function get_all_composition_by_id($id, $select = '*')
-  {
-    // get from tb_department
-    $this->db->select($select);
-    $this->db->from($this->table);
-    $this->db->join($this->tb_product_composition, "{$this->table}.id={$this->tb_product_composition}.product_id");
-    $this->db->join($this->tb_material, "{$this->tb_product_composition}.material_id={$this->tb_material}.id");
-    $this->db->where("{$this->tb_product_composition}.product_id", $id);
-    $query = $this->db->get();
-    if ( $query->num_rows() > 0) {
-      return $query->result_array();
     }
     return FALSE;
   }
--- application/views/kas-perusahaan/v_data-master.php
+++ application/views/kas-perusahaan/v_data-master.php
@@ -1,1 +1,139 @@
-hello
+<div class="content">
+              <div class="page-inner">
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="card">
+
+                      <div class="card-header">
+                        <div class="d-flex align-items-center">
+                          <h4 class="card-title font-weight-bold"><?= $title ?></h4>
+                        </div>
+                      </div>
+
+                      <div class="card-body">
+
+                        <div class="table-responsive">
+                          <table id="table-kas" class="display nowrap table table-sm table-hover">
+                            <thead class="thead-light">
+                              <tr>
+                                <th class="px-3">No</th>
+                                <th class="px-3">Tanggal</th>
+                                <th class="px-3">Perihal</th>
+                                <th class="px-3">Debet</th>
+                                <th class="px-3">Kredit</th>
+                                <th class="px-3">Saldo akhir</th>
+                                <th class="px-3">Tipe</th>
+                                <th class="px-3">Kode</th>
+                                <th class="px-3">Ket. tambahan</th>
+                                <th class="px-3">Aksi</th>
+                                <th></th>
+                              </tr>
+                            </thead>
+                            <tfoot class="thead-light">
+                              <tr>
+                                <th class="px-3">No</th>
+                                <th class="px-3">Tanggal</th>
+                                <th class="px-3">Perihal</th>
+                                <th class="px-3">Debet</th>
+                                <th class="px-3">Kredit</th>
+                                <th class="px-3">Saldo akhir</th>
+                                <th class="px-3">Tipe</th>
+                                <th class="px-3">Kode</th>
+                                <th class="px-3">Ket. tambahan</th>
+                                <th class="px-3">Aksi</th>
+                                <th></th>
+                            </tfoot>
+                            <tbody>
+                              <?php
+                              $i = 1;
+                              foreach ($kas as $row) : ?>
+                                <tr>
+                                  <td class="px-3">
+                                    <?= $i++ ?>
+                                  </td>
+                                  <td class="px-3">
+                                    <?php $d = date_create($row['date']); echo date_format($d,"d-M-Y") ?>
+                                  </td>
+                                  <td class="px-3">
+                                    <?= $row['title'] ?>
+                                  </td>
+                                  <td class="px-3">
+                                    Rp. <?= number_format($row['debet'], 0, '', '.') ?>
+                                  </td>
+                                  <td class="px-3">
+                                    Rp. <?= number_format($row['kredit'], 0, '', '.') ?>
+                                  </td>
+                                  <td class="px-3">
+                                    Rp. <?= number_format($row['final_balance'], 0, '', '.') ?>
+                                  </td>
+                                  <td class="px-3">
+                                    <?= $row['type'] ?>
+                                  </td>
+                                  <td class="px-3">
+                                    <?= $row['kas_code'] ?>
+                                  </td>
+                                  <td class="px-3">
+                                    <?= $row['description'] ?>
+                                  </td>
+                                  
+                                  <td class="">
+                                    <div class="form-button-action d-flex">
+                                      <span data-toggle="tooltip" title="Hapus" data-original-title="Hapus">
+                                        <a href="#modal-delete-data" type="button" data-toggle="modal" data-target="#modal-delete-data" class="p-2 btn-link btn-danger btn-delete" data-id="<?= $row['id'] ?>"><i class="fa fa-times"></i></a>
+                                      </span>
+                                    </div>
+                                  </td>
+                                  <td></td>
+
+                                  <!-- <td class="">
+                                    <div class="form-button-action justify-content-center d-flex">
+                                      <?php
+                                      // <a href=" current_url() . "/edit/{$row['id']}" " class="p-2 btn-link btn-primary" data-toggle="tooltip" title="Atur stok" data-original-title="Atur stok"><i class="fas fa-plus"></i></a>
+                                      // <a href=" current_url() . "/edit/{$row['id']}" " class="p-2 btn-link btn-primary" data-toggle="tooltip" title="Ubah" data-original-title="Ubah"><i class="fa fa-edit"></i></a>
+                                      // <a href=" current_url() . "/detail/{$row['id']}" " class="p-2 btn-link btn-default" data-toggle="tooltip" title="Lihat detail" data-original-title="Lihat detail"><i class="fas fa-eye"></i></a>
+                                      ?>
+                                      <span data-toggle="tooltip" title="Hapus" data-original-title="Hapus">
+                                        <a href="#modal-delete-data" type="button" data-toggle="modal" data-target="#modal-delete-data" class="p-2 btn-link btn-danger btn-delete" data-id="<?= $row['id'] ?>"><i class="fa fa-times"></i></a>
+                                      </span>
+                                    </div>
+                                  </td> -->
+                                </tr>
+                              <?php endforeach; ?>
+                            </tbody>
+                          </table>
+                        </div>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+            <?php // modal untuk hapus data ?>
+            <div class="modal fade" id="modal-delete-data" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h3 class="modal-title text-danger">Konfirmasi Hapus Data</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body my-3">
+                    <h4>Yakin ingin menghapus data?</h4>
+                  </div>
+                  <form action="<?= current_url() . "/hapus" ?>" method="POST">
+                    <div class="modal-footer">
+                      <input type="hidden" name="id" class="id"></input>
+                      <button class="btn btn-danger btn-border">Hapus Data</button>
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <?php // /modal untuk hapus data ?>
